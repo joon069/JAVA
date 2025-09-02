3bb22b082bbe7b7db28c7f70f3afe90a187b7361
강준영 <j8521103@gmail.com>
2025-09-02 22:21:38 +0900

    overload에 대해서 배움

--- src/Main.java
+++ src/Main.java
@@ -1,32 +1,27 @@
+import java.sql.SQLOutput;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-class a4Paper {
-    static int width = 210;
-    static int height = 297;
+public class Main {
+    public static void main(String[] arg) {
 
-    String write;
-    Boolean isOk = true;
-
-    void Ggugigi() {isOk = false;}
-
+        Put p = new Put();
+        p.put("강준영");
+        p.put(17);
+        System.out.println(User.name);
+        System.out.println(User.age);
+    }
+    static class User{
+        static String name = "Unknown";
+        static int age = 0;
+    }
 }
 
-public class Main {
-    public static void main(String[] args) {
-
-        a4Paper p1 = new a4Paper();
-        a4Paper p2 = new a4Paper();
-
-        System.out.println("p1's width " + p1.width);
-        System.out.println("p1's height " + p1.height);
-
-        System.out.println("p2's width " + p2.width);
-        System.out.println("p2's height " + p2.height);
-
-        p1.write = "안녕하세요 저는 1학년 1반 강준영입니다.";
-        p2.write = "안녕하세요 저는 1학년 1반 석준영입니다.";
-
-        System.out.println("p1 :" + p1.write);
-        System.out.println("p2 :" + p2.write);
+class Put {
+    void put(String n){
+        Main.User.name = n;
+    }
+    void put(int n){
+        Main.User.age = n;
     }
 }
