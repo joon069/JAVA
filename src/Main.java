0f63941bc4e207a34c8882acc00d1dc1bee20d58
강준영 <j8521103@gmail.com>
2025-09-09 21:27:08 +0900

    생성자와 생성자 오버로드에 대해서 배움

--- src/Main.java
+++ src/Main.java
@@ -1,27 +1,28 @@
 import java.sql.SQLOutput;
-
+class Dates {
+    String yy;
+    String mm;
+    String dd;
+    Dates() {
+        this("2025", "9", "9");
+    }
+    Dates(String yy, String mm, String dd){
+        this.yy = yy;
+        this.mm = mm;
+        this.dd = dd;
+    }
+    Dates(int x){
+        this("2025", "7", "19");
+    }
+}
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] arg) {
+        Dates d = new Dates();
+        Dates dd = new Dates(1);
 
-        Put p = new Put();
-        p.put("강준영");
-        p.put(17);
-        System.out.println(User.name);
-        System.out.println(User.age);
-    }
-    static class User{
-        static String name = "Unknown";
-        static int age = 0;
-    }
-}
-
-class Put {
-    void put(String n){
-        Main.User.name = n;
-    }
-    void put(int n){
-        Main.User.age = n;
+        System.out.println("오늘의 날짜 : "+ d.yy + "년 " + d.mm + "월 " + d.dd + "일");
+        System.out.println("강준영 생일 : "+ dd.yy + "년 " + dd.mm + "월 " + dd.dd + "일");
     }
 }
