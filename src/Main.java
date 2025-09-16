c06980d8242cf819fe780c3509a22f603e120894
강준영 <j8521103@gmail.com>
2025-09-16 23:25:36 +0900

    과제(이거 이해해서 그릴 수 있을 정도록)

--- src/Main.java
+++ src/Main.java
@@ -1,28 +1,56 @@
-import java.sql.SQLOutput;
-class Dates {
-    String yy;
-    String mm;
-    String dd;
-    Dates() {
-        this("2025", "9", "9");
-    }
-    Dates(String yy, String mm, String dd){
-        this.yy = yy;
-        this.mm = mm;
-        this.dd = dd;
-    }
-    Dates(int x){
-        this("2025", "7", "19");
-    }
-}
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] arg) {
-        Dates d = new Dates();
-        Dates dd = new Dates(1);
+public class  Main {
+    public static void main(String args[]) {
+        Deck d = new Deck();	// 카드 한 벌(Deck)을 만든다.
+        Card c = d.pick(0);	   	// 섞기 전에 제일 위의 카드를 뽑는다.
+        System.out.println(c); 	// System.out.println(c.toString());과 같다.
 
-        System.out.println("오늘의 날짜 : "+ d.yy + "년 " + d.mm + "월 " + d.dd + "일");
-        System.out.println("강준영 생일 : "+ dd.yy + "년 " + dd.mm + "월 " + dd.dd + "일");
+        d.shuffle();			// 카드를 섞는다.
+        c = d.pick(0);		   	// 섞은 후에 제일 위의 카드를 뽑는다.
+        System.out.println(c);
     }
 }
+// Deck클래스
+class Deck {
+    final int CARD_NUM = 52;	// 카드의 개수
+    Card cardArr[] = new Card[CARD_NUM];  // Card객체 배열을 포함
+    Deck () {	// Deck의 카드를 초기화한다.
+        int i=0;
+        for(int k=Card.KIND_MAX; k > 0; k--)
+            for(int n=0; n < Card.NUM_MAX ; n++)
+                cardArr[i++] = new Card(k, n+1);
+    }
+    Card pick(int index) {	// 지정된 위치(index)에 있는 카드 하나를 꺼내서 반환
+        return cardArr[index];
+    }
+    void shuffle() { // 카드의 순서를 섞는다.
+        for(int i=0; i < cardArr.length; i++) {
+            int r = (int)(Math.random() * CARD_NUM);
+
+            Card temp = cardArr[i];
+            cardArr[i] = cardArr[r];
+            cardArr[r] = temp;
+        }
+    }
+} // Deck클래스의 끝
+
+// Card클래스
+class Card {
+    static final int KIND_MAX = 4;	// 카드 무늬의 수
+    static final int NUM_MAX  = 13;	// 무늬별 카드 수
+    static final int SPADE   = 4;
+    static final int DIAMOND = 3;
+    static final int HEART   = 2;
+    static final int CLOVER  = 1;
+    int kind;
+    int number;
+    Card(int kind, int number) {
+        this.kind = kind;
+        this.number = number;
+    }
+    public String toString() {
+        String[] kinds = {"", "CLOVER", "HEART", "DIAMOND", "SPADE"};
+        String numbers = "0123456789XJQK"; // 숫자 10은 X로 표현
+
+        return "kind : " + kinds[this.kind] + ", number : " + numbers.charAt(this.number);
+    } // toString()의 끝
+} // Card클래스의 끝
